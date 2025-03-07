22fb68e45b2b8a7e1d764df7bf97ad801ec123e6
Jchungaz <jchungaz@autonoma.edu.pe>
2025-03-06 21:16:42 -0500

    app.js

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
-document.querySelector('.btn').addEventListener('click', () => {
-    document.querySelector('.btn').style.backgroundColor = '#2ecc71';
-  });
+document.getElementById('submit-btn').addEventListener('click', function(event) {
+  event.preventDefault(); // Evita el envío del formulario
+  const emailInput = document.getElementById('email');
+  const errorIcon = document.getElementById('error-icon');
+  const errorMessage = document.getElementById('error-message');
+  const email = emailInput.value.trim();
+
+  if (!validateEmail(email)) {
+      errorIcon.style.display = 'block';
+      errorMessage.textContent = 'Please provide a valid email';
+      errorMessage.style.display = 'block';
+      emailInput.style.borderColor = 'var(--soft-red)';
+  } else {
+      errorIcon.style.display = 'none';
+      errorMessage.style.display = 'none';
+      emailInput.style.borderColor = 'var(--desaturated-red)';
+      alert('Email is valid!'); // Aquí puedes enviar el formulario o realizar otra acción
+  }
+});
+
+function validateEmail(email) {
+  const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return re.test(String(email).toLowerCase());
+}
